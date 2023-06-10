ba140123e52f43d4d1d41d6e1bfae5537288bf3c
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-06-10 00:06:20 -0300

    chore: delete useless feature

---
+++ build.rs
@@ -0,0 +1,18 @@
+use std::env;
+use std::fs;
+use std::path::Path;
+
+fn main() {
+    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let cwd = env::current_dir().unwrap();
+
+    fs::create_dir_all(&out_dir).expect("Failed to create man directory");
+
+    let src_path = Path::new(&cwd).join("man").join("man1").join("quartz.1");
+    let dest_path = Path::new(&out_dir).join("quartz.1");
+
+    println!("AAAAAAAAAAAAAAAAA");
+    std::process::Command::new("echo").arg(format!("dest: {:?}", dest_path)).spawn();
+
+    fs::copy(&src_path, &dest_path).expect("Failed to copy manpage file");
+}
