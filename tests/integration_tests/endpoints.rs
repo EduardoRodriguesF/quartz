de69011d214d83764eca4ad3dc4cca8a44129dcf
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-07-08 14:46:46 -0300

    test: creates endpoint with url

--- tests/integration_tests/endpoints.rs
+++ tests/integration_tests/endpoints.rs
@@ -21,3 +21,34 @@ fn it_creates_empty_endpoint() -> TestResult {
 
     Ok(())
 }
+
+#[test]
+fn it_creates_endpoint_with_url() -> TestResult {
+    let quartz = Quartz::default();
+    let sample_endpoint = "myendpoint";
+    let sample_url = "https://httpbin.org/get";
+
+    quartz.cmd(&["init"])?;
+
+    let create_result = quartz.cmd(&[
+        "create",
+        sample_endpoint,
+        "--url",
+        sample_url,
+        "--method",
+        "GET",
+    ])?;
+
+    quartz.cmd(&["use", sample_endpoint])?;
+    let output = quartz.cmd(&["url", "--get"])?;
+    let url = String::from_utf8_lossy(&output.stdout);
+
+    assert!(
+        create_result.status.success(),
+        "{}",
+        String::from_utf8_lossy(&create_result.stderr)
+    );
+    assert_eq!(url.trim(), sample_url);
+
+    Ok(())
+}
