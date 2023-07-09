7e2c73e08ae721a1da87c4e7cc68570942e7dd02
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-07-09 18:02:29 -0300

    test: it creates endpoint with method

--- tests/integration_tests/endpoints.rs
+++ tests/integration_tests/endpoints.rs
@@ -37,3 +37,31 @@ fn it_creates_endpoint_with_url() -> TestResult {
 
     Ok(())
 }
+
+#[test]
+fn it_creates_endpoint_with_method() -> TestResult {
+    let quartz = Quartz::default();
+    let sample_endpoint = "myendpoint";
+    let sample_url = "https://httpbin.org/get";
+    let method = "POST";
+
+    quartz.cmd(&["init"])?;
+
+    let create_output = quartz.cmd(&[
+        "create",
+        sample_endpoint,
+        "--url",
+        sample_url,
+        "--method",
+        method,
+    ])?;
+
+    assert!(create_output.status.success(), "{}", create_output.stderr);
+
+    quartz.cmd(&["use", sample_endpoint])?;
+    let method_output = quartz.cmd(&["method", "--get"])?;
+
+    assert_eq!(method_output.stdout.trim(), method);
+
+    Ok(())
+}
