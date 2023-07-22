91a9cf4aff89f31e6622dda5ceece37ee238f2f3
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2023-07-22 12:51:46 -0300

    build: compile pandoc to manpage

--- build.rs
+++ build.rs
@@ -1,15 +1,17 @@
 use std::env;
-use std::fs;
 use std::path::Path;
 
 fn main() {
-    let out_dir = env::var_os("OUT_DIR").unwrap();
+    let mut doc = pandoc::new();
+
     let cwd = env::current_dir().unwrap();
 
-    fs::create_dir_all(&out_dir).expect("Failed to create man directory");
+    let src_path = Path::new(&cwd).join("doc").join("quartz.1.md");
+    let dest_path = Path::new(&cwd).join("doc").join("quartz.1");
 
-    let src_path = Path::new(&cwd).join("man").join("man1").join("quartz.1");
-    let dest_path = Path::new(&out_dir).join("quartz.1");
+    doc.add_input(&src_path);
+    doc.set_output(pandoc::OutputKind::File(dest_path));
+    doc.add_option(pandoc::PandocOption::Standalone);
 
-    fs::copy(src_path, dest_path).expect("Failed to copy manpage file");
+    doc.execute().expect("Failed to compile manpage");
 }
