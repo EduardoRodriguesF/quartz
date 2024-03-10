688baa554c96a5cad755283478f9dc8db54dab80
EduardoRodriguesF <eduardo.rodrigues@econverse.com.br>
2024-03-10 10:04:32 -0300

    test: add git testcase

--- tests/it/init.rs
+++ tests/it/init.rs
@@ -22,3 +22,27 @@ fn it_cant_init_over_other() -> TestResult {
 
     Ok(())
 }
+
+#[test]
+fn init_detect_git() -> TestResult {
+    let quartz = Quartz::default();
+    let binding = quartz.dir();
+    let parent = binding.parent().unwrap();
+
+    // Fake .git
+    std::fs::create_dir(parent.join(".git"))?;
+
+    let output = quartz.cmd(&["init"])?;
+
+    assert!(output.status.success(), "{}", output.stderr);
+    assert!(quartz.dir().exists(), ".quartz was not created");
+    assert!(parent.join(".gitignore").exists(), ".git was not created");
+
+    let gitignore = std::fs::read_to_string(parent.join(".gitignore"))?;
+    assert!(
+        gitignore.contains(".quartz/user"),
+        ".gitignore does not contain .quartz/user"
+    );
+
+    Ok(())
+}
