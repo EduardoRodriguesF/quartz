3c3a37f95fab45b047028e44be72c82436acd208
Gabriel C. Brandão <biel.brandao2004@gmail.com>
2024-07-28 11:37:30 -0300

    refactor(completion): use shell arg as enum instead of string

--- src/action/completion.rs
+++ src/action/completion.rs
@@ -2,10 +2,29 @@ use crate::cli::Cli;
 use clap::{Command, CommandFactory};
 use clap_complete::{generator::generate, Generator, Shell};
 
+#[derive(clap::ValueEnum, Clone, Debug)]
+enum AvailableCompletionShell {
+    Zsh,
+    Bash,
+    Fish,
+    Elvish,
+}
+
+impl Into<Shell> for AvailableCompletionShell {
+    fn into(self) -> Shell {
+        match self {
+            AvailableCompletionShell::Zsh => Shell::Zsh,
+            AvailableCompletionShell::Bash => Shell::Bash,
+            AvailableCompletionShell::Fish => Shell::Fish,
+            AvailableCompletionShell::Elvish => Shell::Elvish,
+        }
+    }
+}
+
 #[derive(Debug, clap::Args)]
 pub struct CompletionArgs {
     #[arg(name = "shell", long = "shell", short)]
-    shell: String,
+    shell: AvailableCompletionShell,
 }
 
 fn get_command() -> Command {
@@ -16,21 +35,8 @@ fn print_completions<G: Generator>(gen: G, cmd: &mut Command) {
     generate(gen, cmd, cmd.get_name().to_string(), &mut std::io::stdout());
 }
 
-fn get_shell_by_name(shell_name: &str) -> Option<Shell> {
-    match shell_name {
-        "zsh" => Some(Shell::Zsh),
-        "bash" => Some(Shell::Bash),
-        "fish" => Some(Shell::Fish),
-        "elvish" => Some(Shell::Elvish),
-        _ => None,
-    }
-}
-
 pub fn cmd(args: CompletionArgs) {
-    if let Some(shell) = get_shell_by_name(&args.shell) {
-        let mut cmd = get_command();
-        print_completions(shell, &mut cmd);
-    } else {
-        eprintln!("no such shell: {}", &args.shell);
-    }
+    let mut cmd = get_command();
+    let shell: Shell = args.shell.into();
+    print_completions(shell, &mut cmd);
 }
